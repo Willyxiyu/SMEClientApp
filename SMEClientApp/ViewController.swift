e91a7badfc231182858208dc8c5de284f459f275
Willyxiyu <Willy.xiyu@gmail.com>
2022-01-04 16:45:15 +0800

    setup the enviroment

---
+++ SMEClientApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SMEClientApp
+//
+//  Created by 林希語 on 2022/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
